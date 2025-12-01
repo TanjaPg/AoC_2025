e13702df2d620abed74939b60593a8de0d48b11c
Georgsson <tanja.georgsson@capgemini.com>
2025-12-01 23:05:38 +0100

    day one, first part

---
+++ Day1/AoCDay1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+var input = File.ReadAllLines(@"input.txt");
+
+var inputList = new List<string>();
+var startValue = 50; 
+int count = 0;
+foreach (string text in input)
+{
+    inputList.Add(text);
+}
+
+var rows = inputList.Count();
+
+foreach (string text in inputList)
+{
+    if(text[0].ToString() == "L")
+    {
+        int number = Int32.Parse(text.Substring(1));
+        startValue = startValue - number;
+        startValue = startValue % 100;
+    }
+    else if(text[0].ToString() == "R")
+    {
+        int number = Int32.Parse(text.Substring(1));
+        startValue = startValue + number;
+        startValue = startValue % 100;
+    }
+
+    if (startValue == 0)
+    {
+        count++;
+    }
+
+}
+Console.WriteLine(count);
